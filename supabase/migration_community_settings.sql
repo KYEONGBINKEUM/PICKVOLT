8e5ef85efc7007db0036f8ab71dc18588c32972d
KYEONGBINKEUM <hn03252@gmail.com>
2026-05-14 09:01:56 +0900

    feat: daily max point limits for post/comment rewards in community settings

--- supabase/migration_community_settings.sql
+++ supabase/migration_community_settings.sql
@@ -6,6 +6,8 @@ CREATE TABLE IF NOT EXISTS app_settings (
 
 -- Default community point rewards
 INSERT INTO app_settings (key, value) VALUES
-  ('community_points_per_post',    '5'),
-  ('community_points_per_comment', '1')
+  ('community_points_per_post',          '5'),
+  ('community_points_per_comment',       '1'),
+  ('community_daily_max_post_points',    '0'),   -- 0 = unlimited
+  ('community_daily_max_comment_points', '0')    -- 0 = unlimited
 ON CONFLICT (key) DO NOTHING;
